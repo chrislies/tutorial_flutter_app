154f25dcb77d8c22281b5a3891fa817ffe23337b
Christopher Lai <clai20707@gmail.com>
2024-03-13 00:10:39 -0400

    Added 'CourseDetails' and 'CallToAction' widgets to home view

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:tutorial_flutter_app/widgets/call_to_action/call_to_action.dart';
 import 'package:tutorial_flutter_app/widgets/centered_view/centered_view.dart';
+import 'package:tutorial_flutter_app/widgets/course_details/course_details.dart';
 import 'package:tutorial_flutter_app/widgets/navigation_bar/navigation_bar.dart';
 
 class HomeView extends StatelessWidget {
@@ -11,7 +14,17 @@ class HomeView extends StatelessWidget {
         backgroundColor: Colors.white,
         body: CenteredView(
           child: Column(
-            children: [NavBar()],
+            children: [
+              NavBar(),
+              Expanded(
+                  child: Row(
+                children: [
+                  CourseDetails(),
+                  Expanded(
+                      child: Center(child: CallToAction(title: "Test Button")))
+                ],
+              )),
+            ],
           ),
         ));
   }
