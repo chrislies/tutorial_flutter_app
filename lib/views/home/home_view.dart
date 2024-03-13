d6a88632b4c893cc47e1895000e8eadfe5018544
Christopher Lai <clai20707@gmail.com>
2024-03-12 21:08:02 -0400

    Wrapped 'Column' with 'CenteredView' widget

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:tutorial_flutter_app/widgets/centered_view/centered_view.dart';
+import 'package:tutorial_flutter_app/widgets/navigation_bar/navigation_bar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+        backgroundColor: Colors.white,
+        body: CenteredView(
+          child: Column(
+            children: [NavBar()],
+          ),
+        ));
   }
 }
